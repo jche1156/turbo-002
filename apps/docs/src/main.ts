7d13d551593c1cf9ea3da785d0ba2172fd941783
John Chen <john.hk.chen@gmail.com>
2023-12-16 20:19:30 -0800

    Remove logos

--- apps/docs/src/main.ts
+++ apps/docs/src/main.ts
@@ -1,18 +1,11 @@
 import { Header } from "@repo/ui/header";
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
 import { Counter } from "@repo/ui/counter";
 import { setupCounter } from "@repo/ui/setup-counter";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    ${Header({ title: "Docs" })}
+    ${Header({ title: "Hello Vite" })}
     <div class="card">
       ${Counter()}
     </div>
